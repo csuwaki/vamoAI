fac59e580d6c87e6fe22b9f8014f41ba744d7d53
Caroline Suwaki <csuwaki@gmail.com>
2021-03-04 18:24:03 -0300

    Adicionando novos códigos

---
+++ modulo1/aula14_for_lista.py
@@ -0,0 +1,4 @@
+ano_nascimento = [23, 52, 33, 64]
+
+for idade in ano_nascimento:
+    print(2021-idade)
---
+++ modulo1/aula14_while_lista.py
@@ -0,0 +1,9 @@
+ver_series = ["Dark", "Community", "Mr. Robot"]
+
+x = 0
+while x < len(ver_series):
+    print(f"A série {ver_series[x]} está na posição {x}. ")
+    x = x + 1
+
+
+
